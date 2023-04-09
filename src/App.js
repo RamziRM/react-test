28ea3f4060f09c6d036e7c32a13cb37655044a85
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-09 00:06:36 -0300

    implement state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import React, {Component} from "react";
+// Handle input field with logic
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: { text: ''},
+      tasks: []
+    };
+  }
+
+  hancdleChange = (e) => {
+    this.setState({
+      task: { text: e.target.value }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setStateState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { text: '' }
+    });
+  };
+  
   render() {
+    const { task, tasks } = this.state;
+
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Task list</h1>
+        <form>
+          <input type="text" id="taskInput" placeholder="Add task" />
+          <button type="submit">Add</button>
+        </form>
       </div>
     );
   }
